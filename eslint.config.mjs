70fee0636b5a4df2e3c2fdbc6501018433f00fdd
awjae <djwotmd@gmail.com>
2026-06-23 21:08:40 +0900

    feature: init docs 보완

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/.next/**'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsparser,
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
